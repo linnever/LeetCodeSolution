dd6449dab0e6df7156bee992f6286482a67534e1
lin <never199410@gmail.com>
2015-11-23 15:39:19 +0800

    7-Reverse-Integer

---
+++ Math/7-Reverse-Integer/Solution.java
@@ -0,0 +1,14 @@
+//Time complexity:O(N) Space O(1)
+public class Solution {
+    public int reverse(int x) {
+        long result =0;
+        while(x != 0)
+        {
+            result = (result*10) + (x%10);
+            if(result > Integer.MAX_VALUE) return 0;
+            if(result < Integer.MIN_VALUE) return 0;
+            x = x/10;
+        }
+        return (int)result;
+    }
+}
